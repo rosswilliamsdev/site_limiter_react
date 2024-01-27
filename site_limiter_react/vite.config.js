02163f12c007b61b02c4bbf1f5f8ebae461f31f3
rosswilliamsdev <116531898+rosswilliamsdev@users.noreply.github.com>
2024-01-27 11:22:46 -0600

    init

---
+++ site_limiter_react/vite.config.js
@@ -0,0 +1,17 @@
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        popup: resolve(__dirname, "popup.html"),
+        options: resolve(__dirname, "options.html"),
+      },
+    },
+  },
+});
